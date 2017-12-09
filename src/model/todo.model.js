4c7dc8d9c650aece3c33bc8ba6f66bdeab6ee3bb
Yuriy-Korovko <korovko.ik@gmail.com>
2017-12-09 14:07:24 +0300

    todo model created

--- src/model/todo.model.js
+++ src/model/todo.model.js
@@ -1,2 +1,11 @@
 export class Todo {
+    id;
+    text;
+    completed;
+
+    constructor(id, text, completed) {
+        this.id = id;
+        this.text = text;
+        this.completed = completed;
+    }
 }
